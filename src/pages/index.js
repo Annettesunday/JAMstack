93cfa314546a6879c4b7408988ea2bfc32e91681
annettesunday <annette.odhiambo@andela.com>
2018-09-03 11:47:52 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Hello, my name is Annette Sunday</p>
+    <p>I am software developer at Andela and I am a lover of all things tech.
+    Glad to be part of this Hackathon</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
